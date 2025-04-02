ab83f1910b8be469fa920e5be8c0a2eaf3256fb3
Tran Viet Vuong <105685671+littlebadboy9x@users.noreply.github.com>
2025-04-02 22:33:55 +0700

    Update function

--- src/main/java/com/example/datn/service/ThuocTinhService/ChatLieuKhungService.java
+++ src/main/java/com/example/datn/service/ThuocTinhService/ChatLieuKhungService.java
@@ -5,14 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
-import java.util.List;
 
 public interface ChatLieuKhungService {
-    List<ChatLieuKhung> findAllChatLieuKhung();
-    ChatLieuKhung findById(Integer id);
+
+    ChatLieuKhung findById(Long id);
     void save(ChatLieuKhung chatLieuKhung);
     Page<ChatLieuKhung> search(String query, Boolean trangThai, Pageable pageable);
     ResponseEntity<?> add(String name);
-    ResponseEntity<?> update(Integer id, String name);
-    ResponseEntity<?> changeStatus(Integer id);
+    ResponseEntity<String> update(Long id, String nameUpdate);
+    ResponseEntity<?> changeStatus(Long id);
 }
