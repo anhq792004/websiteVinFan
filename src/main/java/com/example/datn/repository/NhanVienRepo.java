1de2cc3e799470d1e90fd97f824991127170695f
anhq792004 <nlquocanh204@gmail.com>
2025-07-05 12:09:33 +0700

    update trạng thái nhân viên

--- src/main/java/com/example/datn/repository/NhanVienRepo.java
+++ src/main/java/com/example/datn/repository/NhanVienRepo.java
@@ -1,5 +1,6 @@
 package com.example.datn.repository;
 
+import com.example.datn.entity.KhachHang;
 import com.example.datn.entity.NhanVien.NhanVien;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,19 +10,19 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NhanVienRepo extends JpaRepository<NhanVien,Long> {
-//    @Query("SELECT nv FROM NhanVien nv " +
-//            "WHERE (LOWER(nv.ma) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
-//            "OR LOWER(nv.ten) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
-////            "OR LOWER(kh.taiKhoan.email) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
-//            "OR LOWER(nv.soDienThoai) LIKE LOWER(CONCAT('%', :keyword, '%'))) " +
-//            "AND (:trangThai IS NULL OR nv.trangThai = :trangThai) ")
-//    Page<NhanVien> searchNhanVien(String keyword, Boolean trangThai, Pageable pageable);
-//
-//    @Query("SELECT nv FROM NhanVien nv " +
-//            "WHERE (LOWER(nv.ma) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
-//            "OR LOWER(nv.ten) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
-////            "OR LOWER(kh.taiKhoan.email) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
-//            "OR LOWER(nv.soDienThoai) LIKE LOWER(CONCAT('%', :keyword, '%')))")
-//    Page<NhanVien> searchNhanVienKhongCoTrangThai(String keyword, Pageable pageable);
+    @Query("SELECT nv FROM NhanVien nv " +
+            "WHERE (LOWER(nv.ma) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "OR LOWER(nv.ten) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+//            "OR LOWER(kh.taiKhoan.email) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "OR LOWER(nv.soDienThoai) LIKE LOWER(CONCAT('%', :keyword, '%'))) " +
+            "AND (:trangThai IS NULL OR nv.trangThai = :trangThai) ")
+    Page<NhanVien> searchNhanVien(String keyword, Boolean trangThai, Pageable pageable);
+
+    @Query("SELECT nv FROM NhanVien nv " +
+            "WHERE (LOWER(nv.ma) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "OR LOWER(nv.ten) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+//            "OR LOWER(kh.taiKhoan.email) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "OR LOWER(nv.soDienThoai) LIKE LOWER(CONCAT('%', :keyword, '%')))")
+    Page<NhanVien> searchNhanVienKhongCoTrangThai(String keyword, Pageable pageable);
 
 }
